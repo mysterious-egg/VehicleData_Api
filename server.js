b62b2d6887d8e67480311f43b1e602b42d84cb15
mysterious-egg <71733395+mysterious-egg@users.noreply.github.com>
2024-05-10 00:37:48 +0530

    Gave cors acess

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const db=require("./db.json");
 const port = 3000 || process.env.PORT;
+const cors = require("cors");
+app.use(cors());
 app.get("/api/data/", (req, res) => {
 
     res.send(db);
